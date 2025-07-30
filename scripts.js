fa0e6c419dcadd2b1d60c91412f5740316e6ba70
Joyce Kimani <josphinekimani98@gmail.com>
2025-07-30 22:23:23 +0300

    Update scripts.js to add variables days, maleNames, femaleNames, myGender and Gender. also add if/else

--- scripts.js
+++ scripts.js
@@ -7,4 +7,37 @@ var yy = parseInt(dateOfBirth.slice(2, 4));
 var mm = parseInt(dateOfBirth.slice(5, 7));
 var dd = parseInt(dateOfBirth.slice(8, 10));
 var day = Math.trunc(dayOfTheWeek(cc, mm, yy, dd));
-alert(day);
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var myGender = prompt("Are you male or female?");
+var gender = myGender.toLowerCase()
+if (day === 0 && gender === "male") {
+  alert("Your Akan name is " + maleNames[0] + ".");
+} else if (day === 1 && gender === "male") {
+  alert("Your Akan name is " + maleNames[1] + ".");
+} else if (day === 2 && gender === "male") {
+  alert("Your Akan name is " + maleNames[2] + ".");
+} else if (day === 3 && gender === "male") {
+  alert("Your Akan name is " + maleNames[3] + ".");
+} else if (day === 4 && gender === "male") {
+  alert("Your Akan name is " + maleNames[4] + ".");
+} else if (day === 5 && gender === "male") {
+  alert("Your Akan name is " + maleNames[5] +".");
+  }  else if (day === 6 && gender === "male") {
+    alert("Your Akan name is " + maleNames[6] + ".");
+  } else if (day === 0 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[0] + ".");
+  } else if (day === 1 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[1] + ".");
+  } else if (day === 2 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[2] + ".");
+  } else if (day === 3 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[3] +".");
+  } else if (day === 4 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[4] + ".");
+  } else if (day === 5 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[5] + ".");
+   } else if (day === 6 && gender === "female") {
+    alert("Your Akan name is " + femaleNames[6] + ".");
+   }
