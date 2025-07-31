adcc74448921e09c74ff0844424987a8d91ba675
Joyce Kimani <josphinekimani98@gmail.com>
2025-07-31 14:31:01 +0300

    update scripts.js spacing

--- scripts.js
+++ scripts.js
@@ -10,36 +10,51 @@ var day = Math.trunc(dayOfTheWeek(cc, mm, yy, dd));
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-if (dd > 31 || dd < 1 || mm < 1 || mm >12) {
-  alert("Invalid Date of Birth!");
-} else (myGender = prompt("Are you male or female?"));
+
+      if (dd > 31 || dd < 1 || mm < 1 || mm >12) {
+      alert("Invalid Date of Birth!");
+     }
+      else (myGender = prompt("Are you male or female?"));
 var gender = myGender.toLowerCase()
-if (day === 0 && gender === "male") {
-  alert("Your Akan name is " + maleNames[0] + ".");
-} else if (day === 1 && gender === "male") {
-  alert("Your Akan name is " + maleNames[1] + ".");
-} else if (day === 2 && gender === "male") {
-  alert("Your Akan name is " + maleNames[2] + ".");
-} else if (day === 3 && gender === "male") {
-  alert("Your Akan name is " + maleNames[3] + ".");
-} else if (day === 4 && gender === "male") {
-  alert("Your Akan name is " + maleNames[4] + ".");
-} else if (day === 5 && gender === "male") {
-  alert("Your Akan name is " + maleNames[5] +".");
-  }  else if (day === 6 && gender === "male") {
-    alert("Your Akan name is " + maleNames[6] + ".");
-  } else if (day === 0 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[0] + ".");
-  } else if (day === 1 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[1] + ".");
-  } else if (day === 2 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[2] + ".");
-  } else if (day === 3 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[3] +".");
-  } else if (day === 4 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[4] + ".");
-  } else if (day === 5 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[5] + ".");
-   } else if (day === 6 && gender === "female") {
-    alert("Your Akan name is " + femaleNames[6] + ".");
-   }
+
+      if (day === 0 && gender === "male") {
+       alert("Your Akan name is " + maleNames[0] + ".");
+     }
+      else if (day === 1 && gender === "male") {
+       alert("Your Akan name is " + maleNames[1] + ".");
+     }
+      else if (day === 2 && gender === "male") {
+       alert("Your Akan name is " + maleNames[2] + ".");
+     }
+      else if (day === 3 && gender === "male") {
+       alert("Your Akan name is " + maleNames[3] + ".");
+     }
+      else if (day === 4 && gender === "male") {
+     }
+      else if (day === 5 && gender === "male") {
+       alert("Your Akan name is " + maleNames[5] +".");
+     }
+      else if (day === 6 && gender === "male") {
+       alert("Your Akan name is " + maleNames[6] + ".");
+     }
+      else if (day === 0 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[0] + ".");
+     }
+      else if (day === 1 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[1] + ".");
+     }
+      else if (day === 2 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[2] + ".");
+     }
+      else if (day === 3 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[3] +".");
+     }
+      else if (day === 4 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[4] + ".");
+     }
+      else if (day === 5 && gender === "female") {
+       alert("Your Akan name is " + femaleNames[5] + ".");
+     }
+      else if (day === 6 && gender === "female") {
+      alert("Your Akan name is " + femaleNames[6] + ".");
+     }
